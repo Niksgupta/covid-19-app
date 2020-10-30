932daf311f27e1570a46268b8b4952135d0ffa77
Nikhil Khandelwal <33507803+Niksgupta@users.noreply.github.com>
2020-10-31 00:20:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { useEffect, useState } from "react";
-import logo from './logo.svg';
+// import { useEffect, useState } from "react";
+// import logo from './logo.svg';
 import './App.css';
-import Card from 'react-bootstrap/Card';
-import CardDeck from 'react-bootstrap/CardDeck';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import Form from 'react-bootstrap/form';
-import FormControl from 'react-bootstrap/formControl';
-import Button from 'react-bootstrap/Button';
-import Columns from 'react-columns';
+// import Card from 'react-bootstrap/Card';
+// import CardDeck from 'react-bootstrap/CardDeck';
+// import Navbar from 'react-bootstrap/Navbar';
+// import Nav from 'react-bootstrap/Nav';
+// import Form from 'react-bootstrap/form';
+// import FormControl from 'react-bootstrap/formControl';
+// import Button from 'react-bootstrap/Button';
+// import Columns from 'react-columns';
 import Footer from "./footer";
 import NavFun from "./Nav";
 import HomeFun from "./components/Home";
@@ -25,7 +25,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import {AppBar, ToolBar, IconButton, Typography} from '@material-ui/core';
 // import MenuIcon from '@material-ui/core/Menu';
-import axios from "axios";
+// import axios from "axios";
 
 
 function App() {
